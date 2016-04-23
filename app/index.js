86d3e5c0aaf72d8ace7a181953217783fd17a4a1
Melissa <melissa@Melissas-MacBook-Air.local>
2016-04-23 11:49:56 +0100

    Getting first test component up and running

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Test from './Test';
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).send(
+    renderToString(<Test />)
+  );
 });
 
 app.listen(3000, () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './index.js',
+  entry: './app/index.js',
   target: 'node',
   output: {
       path: __dirname,
@@ -8,7 +8,14 @@ module.exports = {
   module: {
       loaders: [
             { test: /\.css$/, loader: 'style!css' },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            {
+              test: /\.js?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel', // 'babel-loader' is also a legal name to reference
+              query: {
+                presets: ['es2015', 'react']
+              }
+            },
             { test:  /\.json$/, loader: 'json-loader' }
         ]
     }
