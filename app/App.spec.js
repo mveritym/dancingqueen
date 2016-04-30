cff57b88fd151809855ae7235c5f5cc8698bcf19
Melissa <melissa@Melissas-Air.lan>
2016-04-30 14:04:07 +0100

    Adding enzyme and chai-enzyme

--- app/App.spec.js
+++ app/App.spec.js
@@ -1,7 +1,14 @@
-import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+chai.use(chaiEnzyme())
+
+import App from './App';
 
 describe('App Component', () => {
-  it('test', () => {
-    expect(1).to.eq(1);
+  it('renders a header', () => {
+    const app = shallow(<App />);
+    expect(app.find('h1')).to.have.text('Indio');
   });
 });
