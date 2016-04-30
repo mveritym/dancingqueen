292e142e46c2af37dc11675e0d44be900a290d39
Melissa <melissa@Melissas-Air.lan>
2016-04-30 13:26:22 +0100

    Adding react-router

---
+++ app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = (props) => (
+  <div>
+    <h1>Indio</h1>
+    { props.children }
+  </div>
+);
+
+export default App;
--- app/main.js
+++ app/main.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Test from './Test';
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import App from './App';
+import Login from './Login';
 
-render(
-  <Test />,
-  document.getElementById('app')
-);
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={ App }>
+      <IndexRoute component={ Login } />
+    </Route>
+  </Router>
+), document.getElementById('app'))
