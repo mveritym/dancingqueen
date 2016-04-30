ea807dd08bccc513fe724836ac5d96699f933fd5
Melissa Marshall <mveritym@gmail.com>
2016-04-30 19:44:09 +0100

    Adding redux, fixing webpack config to handle babel

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": "\"development\""
+      "process.env.NODE_ENV": "\"development\"",
+      'global': {}
     })
   ]
 };
